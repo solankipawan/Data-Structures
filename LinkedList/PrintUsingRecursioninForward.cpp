c191436b9286bb34c884e226e775f65813aefb20
P_Solanki29 <pksaug2529@gmail.com>
2020-06-30 16:46:23 +0530

    Print linked list using recursion

---
+++ LinkedList/PrintUsingRecursioninForward.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* Insert(Node* head, int data){
+    Node *temp = new Node();
+    temp->data = data;
+    temp->next = NULL;
+    if(head == NULL)
+    {
+     head = temp;
+    }
+    else {
+        Node* temp1 = head;
+        while(temp1->next != NULL)  temp1 = temp1->next;
+        temp1->next = temp;
+        
+    }
+    return head;
+    }
+
+    void print(Node* head){
+        if(head == NULL){// Exit Condition.
+            return;
+        }
+        cout<<head->data<<" ";//frist print the value in the node
+        print(head->next); //Recursive call
+        //If we swap the above two statement so the first call will goto print function after that cout will execute 
+        // so this result the print the element in reverse order See in the next program.
+      
+        
+    }
+
+int main(){
+    Node* head = NULL;  // local variable    
+    head = Insert(head,2);
+    head = Insert(head,3);
+    head = Insert(head,4);
+    head = Insert(head,5);
+    print(head);
+    ;
+}
