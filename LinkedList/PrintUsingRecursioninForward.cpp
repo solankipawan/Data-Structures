81d2a8e239cf8da1e217bdef55500c5e53666074
P_Solanki29 <pksaug2529@gmail.com>
2020-06-30 17:03:08 +0530

    Print linked list using recursion

--- LinkedList/PrintUsingRecursioninForward.cpp
+++ LinkedList/PrintUsingRecursioninForward.cpp
@@ -32,7 +32,9 @@ Node* Insert(Node* head, int data){
         print(head->next); //Recursive call
         //If we swap the above two statement so the first call will goto print function after that cout will execute 
         // so this result the print the element in reverse order See in the next program.
-      
+        
+    /*  For the normal traversal of the linkedList Iterative approch is much better than recursive aaproch because
+     it consume more space in stack at every time of function call.*/
         
     }
 
