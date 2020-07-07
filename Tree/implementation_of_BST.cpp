55ef0dc4b133a3e8726074f941551c74248463e7
P_Solanki29 <pksaug2529@gmail.com>
2020-07-07 17:42:11 +0530

    Implementation of BST

--- Tree/implementation_of_BST.cpp
+++ Tree/implementation_of_BST.cpp
@@ -20,13 +20,43 @@ BSTNode* Insert(BSTNode* root, int data){
     if(root == NULL){ //Empty Tree
         root = GetNewNode(data);
     }
+    else if(data<= root->data){
+        root->left = Insert(root->left, data);
+    }
+    else{
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+bool Search(BSTNode* root, int data){
+    if(root == NULL){
+        return false;
+    }
+    else if(root->data == data){
+        return true;
+    }
+    else if(data<= root->data){
+        return Search(root->left, data);
+    }
+    else {
+        return Search(root->right, data);
+    }
 }
 int main(){
-    BSTNode* root = NULL;
-    root = Insert(root, 2);
-    root = Insert(root, 2);
-    root = Insert(root, 2);
-    root = Insert(root, 2);
-    root = Insert(root, 2);
-    root = Insert(root, 2);
+    BSTNode* root = NULL; // pointer to root node. Setting tree empty.
+    root = Insert(root, 15);
+    root = Insert(root, 10);
+    root = Insert(root, 20);
+    root = Insert(root, 25);
+    root = Insert(root, 8);
+    root = Insert(root, 12);
+    int number;
+    cout<< "Enter the number to be searched\n";
+    cin>> number;
+    if(Search(root, number) == true){
+        cout<<"Found\n";
+    }
+    else{
+        cout<<" Not found\n";
+    }
 }
